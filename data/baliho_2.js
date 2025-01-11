502cad928ab92966f6e0edc77cdd1a804bd365e8
rombenk-ok <aspirenew@gmail.com>
2025-01-12 01:09:53 +0700

    Add files via upload

---
+++ data/baliho_2.js
@@ -0,0 +1,13 @@
+var json_baliho_2 = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                                                                  
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "nama": "Iklan Parpol", "x": 105.240302, "y": -5.372687, "foto": "foto\/01.png" }, "geometry": { "type": "Point", "coordinates": [ 105.240302, -5.372687 ] } },
+{ "type": "Feature", "properties": { "id": "2", "nama": "Iklan Sampo", "x": 105.242063, "y": -5.373947, "foto": "foto\/02.png" }, "geometry": { "type": "Point", "coordinates": [ 105.242063, -5.373947 ] } },
+{ "type": "Feature", "properties": { "id": "3", "nama": "Iklan Sabut", "x": 105.228834, "y": -5.367495, "foto": "foto\/03.png" }, "geometry": { "type": "Point", "coordinates": [ 105.2288344, -5.3674951 ] } },
+{ "type": "Feature", "properties": { "id": "4", "nama": "Gapura Selamat Datang", "x": 105.225811, "y": -5.362102, "foto": "foto\/04.png" }, "geometry": { "type": "Point", "coordinates": [ 105.225811, -5.362102 ] } },
+{ "type": "Feature", "properties": { "id": "5", "nama": "Iklan Mobil", "x": 105.223217, "y": -5.374348, "foto": "foto\/05.png" }, "geometry": { "type": "Point", "coordinates": [ 105.223217, -5.374348 ] } },
+{ "type": "Feature", "properties": { "id": "6", "nama": "Rumah", "x": 105.241823, "y": -5.376104, "foto": "foto\/06.png" }, "geometry": { "type": "Point", "coordinates": [ 105.241823, -5.376104 ] } }
+]
+}
